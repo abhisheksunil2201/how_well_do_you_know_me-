a3684bba8a410fd619c21a8f721095bca2ded4b4
Abhishek <abhisheksunil92@gmail.com>
2021-08-11 21:19:19 +0530

    initial commit

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const readLineSync = require("readline-sync");
 let playerName = readLineSync.question("What is your name?");
 let score = 0;
 
-console.log(`Welcome ${playerName} to How well do you know Abhishek`);
+console.log("Welcome " + playerName + " to How well do you know Abhishek");
 
 function askQuestion(question, ans) {
   let playerResponse = readLineSync.question(question);
   if (playerResponse.toLowerCase() === ans) {
-    console.log("Correct!!!");
     score += 1;
+    console.log("Correct!!!");
+    console.log("Your Current Score is: " + score);
+    console.log("---------------------");
   } else {
     console.log("Incorrect!!!");
+    console.log(
+      "Correct Ans is: " + ans.charAt(0).toUpperCase() + ans.slice(1)
+    );
+    console.log("Your Current Score is: " + score);
+    console.log("---------------------");
   }
 }
 
@@ -18,26 +25,26 @@ function game() {
   let questions = [
     {
       question: "Where does Abhishek live?",
-      ans: "india",
+      answer: "india",
     },
     {
       question: "What is his favourite game?",
-      ans: "football",
+      answer: "football",
     },
     {
       question: "What does Abhishek like to eat the most?",
-      ans: "icecream",
+      answer: "icecream",
     },
     {
       question: "Who is Abhishek's favourite football player?",
-      ans: "cristiano ronaldo",
+      answer: "cristiano ronaldo",
     },
   ];
 
-  questions.map(({ question, ans }) => {
-    askQuestion(question, ans);
-  });
-
+  for (let i = 0; i < questions.length; i++) {
+    let ques = questions[i];
+    askQuestion(ques.question, ques.answer);
+  }
   console.log();
   console.log("Thank you for playing the game!!");
   console.log(`Your score is:${score}/4`);
